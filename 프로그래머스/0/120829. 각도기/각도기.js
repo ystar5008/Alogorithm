71eb750b8d209df95b32559f9ff413d273866c51
index.ys <96641210+ystar5008@users.noreply.github.com>
2024-10-23 19:16:34 +0900

    [level 0] Title: 각도기, Time: 3.46 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    if (angle < 90) {
+        console.log(angle)
+        return 1
+    } else if (angle === 90) {
+        return 2
+    } else if (angle < 180) {
+        return 3
+    } else {
+        return 4
+    }
+
+}
